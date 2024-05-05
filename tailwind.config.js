f2d87358a2afea34de90a5b84767cfbd242f20cc
yerrouhamza <hamzayerrou5@yahoo.com>
2024-05-05 14:51:04 +0100

    setuo the app them plates main colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     fontFamily: {
       'montserrat': ['"Montserrat Alternates"', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        'theme-davys-grey': '#4D4D4D', 
+        'theme-vermillion': '#CD4631',
+        'theme-pewter-blue': '#81ADC8',
+        'theme-champagne': '#F7EDE8',
+        'theme-alice-blue': '#EDF3F7',
+      }
+    },
   },
   variants: {
     extend: {},
