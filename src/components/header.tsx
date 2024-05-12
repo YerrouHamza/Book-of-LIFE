e4fce257481a9d123dd2cbafc1f2cc4e33cf48c7
yerrouhamza <hamzayerrou5@yahoo.com>
2024-05-12 15:28:59 +0100

    imporved the hero assets & styled the hero asstes & improved header component by addded the comments

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,12 @@
 import Navbar from './navbar.tsx';
 import Button from './button.tsx';
 
+// import icons
+import HeroStar from '../assets/shapes/hero-stare.png';
+import HeroSwirl from '../assets/shapes/Hero-swirl.svg';
+
+
+// Define the props type for the header component
 type headerProps = {
   title: {
     first: String,
@@ -11,7 +17,7 @@ type headerProps = {
 }
 
 const Header = (props: headerProps) => {
-
+  // Define the navbar props pages
   const navbarProps = [
     {
       title: 'Home',
@@ -29,8 +35,8 @@ const Header = (props: headerProps) => {
 
   return (
     <header className="bg-theme-champagne py-10">
-        {props.isNavbarShow ? <Navbar pages={navbarProps} /> : null}
-        <div className='m-auto max-w-screen-xl mt-10 flex flex-col justify-center items-center'>
+        {props.isNavbarShow && navbarProps.length > 0 ? <Navbar pages={navbarProps} /> : null}
+        <div className='m-auto max-w-screen-xl mt-10 flex flex-col justify-center items-center relative'>
 
           {props.title?.first ? 
             <h1 className='text-center text-7xl font-bold leading-tight'>
@@ -42,6 +48,9 @@ const Header = (props: headerProps) => {
           {props.description ? <p className='text-center text-lg mt-10 mx-auto w-1/3'>{props.description}</p> : null}
           
           <Button text='SUBSCRIBE' variant='dark' customClass='mx-auto mt-10' />
+
+          <img className='hero-svg absolute right-3 w-32' src={HeroStar} alt='hero star shape' />
+          <img className='hero-svg absolute -left-10 w-60' src={HeroSwirl} alt='hero swirl shape' />
         </div>
     </header>
   );
