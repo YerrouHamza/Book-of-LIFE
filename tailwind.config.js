4618fa068aafe0010fde76f023f5fd6df52ec715
yerrouhamza <hamzayerrou5@yahoo.com>
2024-05-17 18:12:10 +0100

    updated the tailwind config to x3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
   theme: {
     fontFamily: {
       'montserrat': ['"Montserrat Alternates"', 'sans-serif']
