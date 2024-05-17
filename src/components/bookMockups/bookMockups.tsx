705bcbd16b7e4716cdf907a30befa2e8ce6f048a
yerrouhamza <hamzayerrou5@yahoo.com>
2024-05-17 19:58:38 +0100

    improved the book mockups component style and props by taypscript

--- src/components/bookMockups/bookMockups.tsx
+++ src/components/bookMockups/bookMockups.tsx
@@ -1,21 +1,19 @@
 
 // import bookMockups styles
 import './bookMockups.scss';
-import cover from '../../assets/book-cover.png';
 
-const BookMockups = () => {
+
+type BookMockupsProps = {
+    bookCover: string,
+    bookLink: string,
+    altBookCover: string
+}
+
+const BookMockups = (props: BookMockupsProps) => {
     return (
-        <a 
-            className="book-container"
-            href=""
-            target="_blank"
-            rel="noreferrer noopener"
-            >
+        <a className="book-container" href={props.bookLink} target="_blank">
             <div className="book">
-                <img
-                alt=""
-                src={cover}
-                />
+                <img alt={props.altBookCover} src={props.bookCover} />
             </div>
         </a>
     );
